86bb0b7e43baae3702990cae6b3178311a84f204
Chosen_1st <haizhongdelan@outlook.com>
2023-08-15 03:48:13 +0800

    Add docs

--- docs/.vuepress/.temp/register-components/clientConfig.23d5a372.js
+++
@@ -1,8 +0,0 @@
-import { defineAsyncComponent } from 'vue'
-
-export default {
-  enhance: ({ app }) => {    
-      app.component("nav-card-item", defineAsyncComponent(() => import("C:/Git/Skydom-Wiki/docs/.vuepress/components/nav-card-item.vue"))),
-      app.component("nav-card", defineAsyncComponent(() => import("C:/Git/Skydom-Wiki/docs/.vuepress/components/nav-card.vue")))
-  },
-}
